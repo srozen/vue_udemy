145226d5b7d18d03b33f3bb96b1817b1640cb410
Samuel Monroe <spat.monroe@gmail.com>
2019-01-12 23:43:34 +0900

    Finished components examples

---
+++ 7_components_intro/vue-cli/src/Home.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="">
+    <app-server-status v-for="server in 5" :key="server.id"></app-server-status>
+  </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue'
+export default {
+  components: {
+    'app-server-status': ServerStatus
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ 7_components_intro/vue-cli/src/ServerStatus.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div class="">
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus" type="button" name="button">Change Status</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+    changeStatus(){
+      this.status = 'Normal';
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
