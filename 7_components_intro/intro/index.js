f469c96c6c44c30b6652c9b717037acb11560717
Samuel Monroe <spat.monroe@gmail.com>
2019-01-12 21:08:53 +0900

    Intro to components

---
+++ 7_components_intro/intro/index.js
@@ -0,0 +1,27 @@
+//var data = { status: 'Critical' }
+var cmp = {
+  data: function(){
+    return {
+      status: 'Critical'
+    }
+  },
+  template: '<p> Server Status : {{ status }} <button @click="changeStatus">Change</button></p>',
+  methods: {
+    changeStatus: function(){
+      this.status = 'Normal'
+    }
+  }
+};
+new Vue({
+  el: '#app',
+  components: {
+    'my-cmp': cmp
+  }
+});
+
+new Vue({
+  el: '#app2',
+  components: {
+    'my-cmp2': cmp
+  }
+});
