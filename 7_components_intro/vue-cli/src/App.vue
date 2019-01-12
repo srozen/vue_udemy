6bce76aa3bf34f083e8a2fbd7d00202ee1336187
Samuel Monroe <spat.monroe@gmail.com>
2019-01-13 01:05:10 +0900

    Module 7 complete

--- 7_components_intro/vue-cli/src/App.vue
+++ 7_components_intro/vue-cli/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <app-servers></app-servers>
+  <div class="container">
+        <app-header></app-header>
+        <hr>
+        <app-servers></app-servers>
+        <app-server-details></app-server-details>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Servers from './components/Servers.vue'
+import ServerDetails from './components/ServerDetails.vue'
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-servers': Servers,
+    'app-server-details': ServerDetails
+  }
+}
 </script>
 
 <style>
+
 </style>
