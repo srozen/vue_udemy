d3d1a86c2b14d3da635688edb1154e701356e40b
Samuel Monroe <spat.monroe@gmail.com>
2019-01-13 00:40:13 +0900

    Exercise 6 complete

--- 7_components_intro/exercise6/src/App.vue
+++ 7_components_intro/exercise6/src/App.vue
@@ -1,39 +1,24 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+  <div class="container">
+        <app-header></app-header>
         <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
+        <app-main></app-main>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import ServerMain from './ServerMain.vue'
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-main': ServerMain
+  }
+}
 </script>
 
 <style>
---
+++ 7_components_intro/exercise6/src/ServerMain.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="row">
+    <server-list></server-list>
+    <server-details></server-details>
+  </div>
+</template>
+
+<script>
+import ServerDetails from './ServerDetails.vue'
+import ServerList from './ServerList.vue'
+export default {
+  components: {
+    'server-list': ServerList,
+    'server-details': ServerDetails
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
