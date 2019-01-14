2dd79ac6d239d4c9bdffe2ac52a20f372205c31b
Samuel Monroe <spat.monroe@gmail.com>
2019-01-14 14:31:50 +0900

    Exercise 7 complete and module 8 complete

---
+++ 8_communicating_components/exercise7/src/components/Server/Server.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <li class="list-group-item" @click="displayDetails">
+      Server #{{ server.id }} | Status : {{ server.status }}
+  </li>
+</template>
+
+<script>
+import { eventBus } from '../../main.js'
+export default {
+  props: ['server'],
+  methods: {
+    displayDetails(){
+      eventBus.displayDetails(this.server)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ 8_communicating_components/exercise7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <div v-if="currentServer">
+      <h2>Server #{{ this.currentServer.id }}</h2>
+      <p>Status : {{ this.currentServer.status}}</p>
+      <button type="button" name="button" @click="restoreStatus">Restore Status</button>
+    </div>
+    <div v-else>No server selected</div>
+  </div>
+
+</template>
+
+<script>
+import { eventBus } from '../../main.js'
+export default {
+  data: function(){
+    return {
+      currentServer: null
+    }
+  },
+  created(){
+    eventBus.$on('displayDetails', (data) => {
+      this.currentServer = data;
+    })
+  },
+  methods: {
+    restoreStatus(){
+      this.currentServer.status = 'Normal'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
