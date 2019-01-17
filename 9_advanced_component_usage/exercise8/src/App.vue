f32a2eb01024259dfae464b343233f3a8710dfc8
Samuel Monroe <spat.monroe@gmail.com>
2019-01-17 20:56:12 +0900

    Exercise 8 and module 9 finished

---
+++ 9_advanced_component_usage/exercise8/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedComponent = 'appBlue'"class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedComponent = 'appGreen'"class="btn btn-success">Load Green Template</button>
+                <button @click="selectedComponent = 'appRed'"class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <component :is="selectedComponent">
+                    This will be displayed in the slot ! 
+                </component>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: function(){
+          return {
+            selectedComponent: 'appBlue'
+          }
+        }
+    }
+</script>
+
+<style>
+</style>
