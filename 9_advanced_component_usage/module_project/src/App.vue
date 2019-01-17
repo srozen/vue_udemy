00308842fd692223fc6353cfab83893d512acbff
Samuel Monroe <spat.monroe@gmail.com>
2019-01-17 20:47:11 +0900

    Learning project for module 9

---
+++ 9_advanced_component_usage/module_project/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <button @click="selectedComponent = 'appQuote'"ype="button" name="button">Quote</button>
+          <button @click="selectedComponent = 'appAuthor'"ype="button" name="button">Author</button>
+          <button @click="selectedComponent = 'appNew'"ype="button" name="button">New</button>
+
+          <hr>
+          <p>{{ selectedComponent }}</p>
+          <keep-alive>
+            <component :is="selectedComponent">
+              <p>Default Content</p>
+            </component>
+          </keep-alive>
+          <!--
+          <app-quote>
+            <h1 slot="title">{{ quoteTitle }}</h1>
+            <p>A wonderful quote</p>
+          </app-quote>
+          -->
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+import New from './components/New.vue'
+import Author from './components/Author.vue'
+
+export default {
+  data: function(){
+    return {
+      quoteTitle: 'The Quote',
+      selectedComponent: 'appQuote'
+    }
+  },
+  components: {
+    appQuote: Quote,
+    appNew: New,
+    appAuthor: Author
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 9_advanced_component_usage/module_project/src/components/New.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <div>
+    <h3>New Quote</h3>
+    <button @click="displayed = !displayed" type="button" name="button"> Display me !</button>
+    <div v-if="displayed">
+      Hello !
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return{
+      displayed: false
+    }
+  },
+  destroyed(){
+    console.log("DESTROYED !!")
+  },
+  deactivated(){
+    console.log("DEACTIVATED !!")
+  },
+  activated(){
+    console.log("ACTIVATED !!")
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
