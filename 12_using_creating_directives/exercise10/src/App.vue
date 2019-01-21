621cdb098d173921d29cac45d867a55637c03f93
Samuel Monroe <spat.monroe@gmail.com>
2019-01-21 15:28:59 +0900

    Exercise 10 finished

--- 12_using_creating_directives/exercise10/src/App.vue
+++ 12_using_creating_directives/exercise10/src/App.vue
@@ -1,19 +1,35 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Directives Exercise</h1>
-                <!-- Exercise -->
-                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
-
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <p v-myOn:dblclick="myAlert">Click on me pls</p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {}
+  export default {
+    directives: {
+      'myOn': {
+        bind(el, binding, vnode){
+          el.addEventListener(binding.arg, binding.value)
+        }
+      }
+    },
+    methods: {
+      myAlert(){
+        alert('Fooo')
+      }
+    }
+  }
 </script>
 
 <style>
+  p {
+    border: 1px solid black;
+  }
 </style>
