67637518c7415bb5097a76c43861e29b0eb08b17
Samuel Monroe <spat.monroe@gmail.com>
2019-01-26 19:21:54 +0900

    Filters and mixins module code

---
+++ 13_filters_and_mixins/filters_project/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        {{ text | toUppercase | to-lower-case }}
+        <hr>
+        <input v-model="filterText">
+        <ul>
+          <li v-for="fruit in filteredFruits" > {{ fruit }} </li>
+        </ul>
+        <button @click="removeObject" name="button"></button>
+      </div>
+      <app-list></app-list>
+    </div>
+  </div>
+</template>
+
+<script>
+  import List from './List.vue'
+  import {fruitMixin} from './fruitMixin.js'
+
+  export default {
+    mixins: [fruitMixin],
+    components: {
+      'app-list' : List
+    },
+    data(){
+      return {
+        text: "Hello there!"
+      }
+    },
+    methods: {
+      removeObject(){
+        this.fruits.pop()
+      }
+    },
+    filters: {
+      toUppercase(value){
+        return value.toUpperCase()
+      },
+      toLowercase(value){
+        return value.toLowerCase()
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ 13_filters_and_mixins/filters_project/src/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <input v-model="filterText">
+        <ul>
+          <li v-for="fruit in filteredFruits" > {{ fruit }} </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {fruitMixin} from './fruitMixin.js'
+  export default {
+    mixins: [fruitMixin],
+    created(){
+      console.log('List created()')
+    }
+  }
+</script>
+
+<style>
+
+</style>
