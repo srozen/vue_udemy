65a61d8adf819f8991f8b0a6c3fccca7f22ecde3
Samuel Monroe <spat.monroe@gmail.com>
2019-01-27 13:33:47 +0900

    Animation properties on elements

--- 14_animations_transitions/module_code/src/App.vue
+++ 14_animations_transitions/module_code/src/App.vue
@@ -11,6 +11,12 @@
             This is some info
           </div>
         </transition>
+
+        <transition name="slide">
+          <div v-if="show" class="alert alert-info">
+            This is some info
+          </div>
+        </transition>
       </div>
     </div>
   </div>
@@ -32,7 +38,7 @@ export default {
   }
 
   .fade-enter-active {
-    transition: opacity 0.5s;
+    transition: opacity 1s;
   }
 
   .fade-leave {
@@ -40,7 +46,40 @@ export default {
   }
 
   .fade-leave-active {
-    transition: opacity 0.5s;
+    transition: opacity 1s;
     opacity: 0;
+  }
+
+  .slide-enter {
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
   }
 </style>
