ec6dd806db08b530592d528a2a437f365f1a934c
Samuel Monroe <spat.monroe@gmail.com>
2019-01-27 13:27:21 +0900

    Transitioning on opacity

---
+++ 14_animations_transitions/module_code/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button @click="show = !show" class="btn btn-primary" name="button">Show alert</button>
+        <br><br>
+        <transition name="fade">
+          <div v-if="show" class="alert alert-info">
+            This is some info
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  }
+}
+</script>
+
+<style>
+  .fade-enter {
+    opacity: 0;
+  }
+
+  .fade-enter-active {
+    transition: opacity 0.5s;
+  }
+
+  .fade-leave {
+
+  }
+
+  .fade-leave-active {
+    transition: opacity 0.5s;
+    opacity: 0;
+  }
+</style>
