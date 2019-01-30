e5acc6483123bdda85474f10202c7b4a308af920
Samuel Monroe <spat.monroe@gmail.com>
2019-01-30 19:25:07 +0900

    Navigating through links

--- 16_routing/module_project/src/App.vue
+++ 16_routing/module_project/src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
+        <app-header></app-header>
         <hr>
         <router-view></router-view>
       </div>
@@ -11,7 +12,12 @@
 </template>
 
 <script>
+import Header from './Header.vue'
+
 export default {
+  components: {
+    'app-header': Header
+  }
 }
 </script>
 
---
+++ 16_routing/module_project/src/Header.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <router-link to="/">Home</router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="user">User</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
