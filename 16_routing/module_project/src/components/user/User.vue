ddf57e500b4223bdbb4ffc54e837f0e2a11e0c0a
Samuel Monroe <spat.monroe@gmail.com>
2019-01-30 20:12:59 +0900

    Imperative routing

--- 16_routing/module_project/src/components/user/User.vue
+++ 16_routing/module_project/src/components/user/User.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>The User Page</h1>
+  <div>
+    <h1>The User Page</h1>
+    <hr>
+    <button class="btn btn-primary" @click="navigateToHome">Go to home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navigateToHome(){
+      this.$router.push('/')
+    }
+  }
+}
+</script>
