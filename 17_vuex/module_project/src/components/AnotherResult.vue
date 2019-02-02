d896e6c1de5b894973ec2d9201ad10542b27a312
Samuel Monroe <spat.monroe@gmail.com>
2019-02-02 17:24:24 +0900

    Using getters and installed Babel preset

---
+++ 17_vuex/module_project/src/components/AnotherResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Counter is: {{ counter }}</p>
+    <p>Number of clicks : {{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters({
+      counter: 'doubleCounter',
+      clicks: 'stringCounter'
+    })
+  }
+}
+</script>
