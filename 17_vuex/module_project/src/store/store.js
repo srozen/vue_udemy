e716579726cc27553062b60e2aba61722c35a32b
Samuel Monroe <spat.monroe@gmail.com>
2019-02-02 18:24:45 +0900

    Using mapGetters, mapMutations, mapActions

--- 17_vuex/module_project/src/store/store.js
+++ 17_vuex/module_project/src/store/store.js
@@ -14,5 +14,31 @@ export const store = new Vuex.Store({
     stringCounter: state => {
       return state.counter + ' Clicks'
     }
+  },
+  mutations: {
+    increment: state => {
+      state.counter++
+    },
+    decrement: state => {
+      state.counter--
+    }
+  },
+  actions: {
+    increment: context => {
+      context.commit('increment')
+    },
+    decrement: ({commit}) => {
+      commit('decrement')
+    },
+    asyncIncrement: ({commit}) => {
+      setTimeout(() => {
+        commit('increment')
+      }, 1000)
+    },
+    asyncDecrement: ({commit}) => {
+      setTimeout(() => {
+        commit('decrement')
+      }, 1000)
+    }
   }
 })
