13c50184e2e4fcfc1d68cdb57d1c847721891b36
Samuel Monroe <spat.monroe@gmail.com>
2019-02-09 12:30:47 +0900

    Added babel presets for Vuex

--- 18_final_project/final_project/src/App.vue
+++ 18_final_project/final_project/src/App.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="container">
+  <div class="container">
 
-    </div>
+  </div>
 </template>
 
 <script>
+export default {
 
-    export default {
-
-    }
+}
 </script>
 
 <style>
