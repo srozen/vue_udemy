21671fa3864de58bbdba8412e643b5ecb1a94938
Samuel Monroe <spat.monroe@gmail.com>
2019-02-09 15:27:45 +0900

    Displaying stocks with placeholder data

--- 18_final_project/final_project/src/components/stocks/Stocks.vue
+++ 18_final_project/final_project/src/components/stocks/Stocks.vue
@@ -1,9 +1,27 @@
 <template lang="html">
-  <h1>The Stocks Component</h1>
+  <div class="">
+    <h1>The Stocks Component</h1>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
+import Stock from './Stock.vue'
+
 export default {
+  data(){
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 110},
+        {id: 2, name: 'Google', price: 200},
+        {id: 3, name: 'Apple', price: 250},
+        {id: 4, name: 'Twitter', price: 8}
+      ]
+    }
+  },
+  components: {
+    'app-stock': Stock
+  }
 }
 </script>
 
