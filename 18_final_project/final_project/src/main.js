37953a33b2cccfa2ed6798b0e9f68f0d406f0c97
Samuel Monroe <spat.monroe@gmail.com>
2019-02-21 14:48:59 +0900

    Setting up VueResource

--- 18_final_project/final_project/src/main.js
+++ 18_final_project/final_project/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import { routes } from './routes.js'
 import store from './store/store.js'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vuetest-7c1c8.firebaseio.com/'
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString()
