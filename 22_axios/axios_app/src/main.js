8a70730cbdaa28549764002da76e9245e38f9bf6
Samuel Monroe <spat.monroe@gmail.com>
2019-02-22 21:09:58 +0900

    Interceptors

--- 22_axios/axios_app/src/main.js
+++ 22_axios/axios_app/src/main.js
@@ -8,8 +8,20 @@ import store from './store'
 
 axios.defaults.baseURL = 'https://vuetest-7c1c8.firebaseio.com'
 axios.defaults.headers.common['Authorization'] = 'something'
-axios.defaults.headers.get['Accept'] = 'application/json' // Targets only get requests
+axios.defaults.headers.get['Accepts'] = 'application/json' // Targets only get requests
 
+const reqInt = axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers['Custom'] = 'foo'
+  return config
+})
+const resInt = axios.interceptors.response.use(res => {
+  console.log(res)
+  return res
+})
+
+axios.interceptors.request.eject(reqInt)
+axios.interceptors.request.eject(resInt)
 new Vue({
   el: '#app',
   router,
