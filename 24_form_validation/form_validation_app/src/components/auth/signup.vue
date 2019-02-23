50b05b4840bf51a534a97d0a62b71b7a42006a2b
Samuel Monroe <spat.monroe@gmail.com>
2019-02-23 20:43:24 +0900

    Validating arrays

--- 24_form_validation/form_validation_app/src/components/auth/signup.vue
+++ 24_form_validation/form_validation_app/src/components/auth/signup.vue
@@ -53,14 +53,18 @@
             <div
                     class="input"
                     v-for="(hobbyInput, index) in hobbyInputs"
+                    :class="{invalid: $v.hobbyInputs.$each[index].$error}"
                     :key="hobbyInput.id">
               <label :for="hobbyInput.id">Hobby #{{ index }}</label>
               <input
                       type="text"
                       :id="hobbyInput.id"
+                      @blur="$v.hobbyInputs.$each[index].value.$touch()"
                       v-model="hobbyInput.value">
               <button @click="onDeleteHobby(hobbyInput.id)" type="button">X</button>
             </div>
+            <p v-if="!$v.hobbyInputs.minLen">You have to specify at least {{$v.hobbyInputs.$params.minLen.min }} hobbies.</p>
+            <p v-if="!$v.hobbyInputs.required">Add at least one hobby.</p>
           </div>
         </div>
         <div class="input inline" :class="{invalid: $v.terms.$invalid}">
@@ -115,6 +119,16 @@
         required: requiredUnless(vm => {
           return vm.country === 'germany'
         })
+      },
+      hobbyInputs: {
+        required,
+        minLen: minLength(2),
+        $each: {
+          value: {
+            required: required,
+            minLen: minLength(5)
+          }
+        }
       }
     },
     methods: {
